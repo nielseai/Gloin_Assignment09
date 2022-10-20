89f583f230e5d4c79cd727040ff2662cd40be093
ariar <ariar@DESKTOP-PMIQ0CQ.wifi.uc.edu>
2022-10-20 09:07:49 -0400

    Added Documentation

--- ClassPackages/Classes.py
+++ ClassPackages/Classes.py
@@ -1,5 +1,10 @@
 '''
-Created on Oct 20, 2022
-
-@author: ariar
+Name: Alex Bridge, Jon Buck, Grace Hertzfeld, Aria Nielsen
+email:  # everyone put their email
+Assignment: Assignment 09
+Course: IS 4010
+Semester/Year: Fall 2022
+Brief Description: This assignment demonstrates that we can develop a project with classes to model something 
+in the real world using github and eclipse
+Anything else that's relevant:
 '''
