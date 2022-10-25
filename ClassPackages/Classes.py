f416c33bbe621646904348a10d78045184a617ee
grace <grace@GraceDell>
2022-10-25 07:56:10 -0400

    Created Pop class module

--- ClassPackages/Classes.py
+++ ClassPackages/Classes.py
@@ -9,22 +9,3 @@ in the real world using github and eclipse
 Anything else that's relevant:
 '''
 
-class Pop():
-    def validateBrand(self, brand):
-        brands = ["Coca-Cola", "PepsiCo"]
-        if brand in brands:
-            self.brand = brand
-        else:
-            print("This is not a pop brand")
-    def __init__(self, brand, name):    
-        self.brand = brand
-        self.name = name              
-        self.validateBrand(brand)
-    def __repr__(self):
-        return "brand = " + self.brand + "; name = " + self.name
-    def __str__(self):
-        return "brand = " + self.brand + "; name = " + self.name
-    def get_name(self):
-        return self.get_name
-    def set_name(self, x):
-        self._name = x
